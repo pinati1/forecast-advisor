146bf75bbfbd83e1127224ad4bd1639202978623
pinati1 <789ori1@gmail.com>
2026-03-14 17:49:44 +0200

    add beach model and pydantic dependencie

---
+++ backend/models/Beach.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class Beach(BaseModel):
+    id: str
+    name: str
+    location: str
+    latitude: float
+    longitude: float
+    country: str
+    city: str
